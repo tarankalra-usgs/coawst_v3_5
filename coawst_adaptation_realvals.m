b7355e7e6ca76e5807bac06f55f1f28a231665e2
taran <tarankalra@gmail.com>
2021-01-27 17:10:49 -0500

    matlab files added

---
+++ coawst_adaptation_realvals.m
@@ -0,0 +1,115 @@
+clear all ; close all ; clc ; 
+% iterate through time
+% Adaptation of growht curve to COAWST model ..need to read a mat file
+% with information of mean high water and a marsh elevation (or bathy that
+% would be the depth % 
+% currently the mat file has information on two points 
+
+%
+% 
+sec2day=1.0/86400; 
+dt=3600; 
+por=.9; % #rough estimate of 1-porosity
+dens=2650; 
+chiref=0.158; %# recalcitrant carbon
+po=(1-por)*dens ; % 300; % *(1-por)*density ;% 
+bedmass(1:721)=250; % Initial bedmass of organic matter in kg/sq-m
+marsh_grow(1:721)=0.0 ; 
+marsh_mask=1 ;
+
+
+% I have to double check but I recall nuGp has units [1/day] 
+%AMC then has units kg/[s m^2 ]  (e.g. the total growth for 180 days, is resealed to a yearly rate in seconds)
+%then (kg/[s m^2]) /( kg/[m^3]) = m/s  which is the vertical accretion rate as appropriate
+%As for the scaling factor, this was a simple math excercise.
+%It will take me time to recreate but basically, when the 
+%derivative of the parabola is zero you want the function to evaluate at 
+%1 given the two roots Dmax and Dmin of the parabola
+% 
+% 
+% 
+load('matfiles/point12_data_mvt','zeta1','dmin1','mvt1',.....
+                        'mbp1','mtr1','h1',........
+                        'zeta2','dmin2','mvt2',.....
+                        'mbp2','mtr2','h2'); 
+ marsh_grow_old = 0.0 ; 
+ for iic=1:length(dmin1)  ; % length(Dmin)
+%   %disp('dt in days')
+   dt_indays(iic)=iic*dt*sec2day ;
+   BMax=2.500; %# kg/m2
+   
+   Dmin=dmin2(iic)     ; % Read in MHW)
+   
+   Dmax=-0.73*2.0*Dmin+0.092+Dmin; % 
+   
+   dmax2(iic)=Dmax     ;
+   Depth=-0.05 ;%zeta1(iic)    ; % you know i wrote my thoughts about our onr work, but nobody talks science together so i kept that as a note.. because like you said John was borderline weird to me that pissed me off .. in order to fill him with neils work , i showed him reedy creek sims and wave thrust. now the waves there are 0.001 mm in height ..  
+   
+   Depth=abs(Depth); 
+   AA=0.25*(Dmin-Dmax)*(Dmax-Dmin)          ; 
+   Bpeak=BMax*(Depth-Dmax)*(Depth-Dmin)/AA  ;% #peak biomass as a function of depth
+%   
+   Bpeak(Bpeak<=1e-9)=0  ;
+%   Bfrac=(Bpeak/BMax);  % #quick fraction for plotting
+%   
+   Bpeak=max(Bpeak,0.0) ; 
+   nuGp=.0138 ; % accordign to Joel units of 1/day #below ground  fraction 
+% % % 
+   if(dt_indays(iic)<180)
+% %   AMC then has units kg/[year m^2 ] % Bpeak is in kg/sq.m and nugp is in
+% %   1/day , *dt_indays = kg/(m^2-year)
+%    AMC(iic)=dt_indays(iic)*Bpeak*(nuGp)/(365*24*3600);  %180 growing days
+%
+%    AMC(iic)=dt_indays(iic)*Bpeak*(nuGp);  %180 growing days
+    AMC(iic)=Bpeak*(nuGp);  %180 growing days
+
+%    % only do this for marsh cells 
+    AMC(iic)=AMC(iic)*marsh_mask  ;
+   else
+    AMC(iic)=0.0; 
+   end 
+   
+   Rref(iic)=AMC(iic)*chiref  ;%#what material remains % kg*/(m^2-year)
+   marsh_rate_vert(iic)=(Rref(iic)/po);%  #vertical accretion rate in m/year
+   
+   % if this the rate per year, then convert it to seconds first m/s
+   marsh_vert_rate_insec(iic)= marsh_rate_vert(iic)/(365*86400)       ; 
+   
+   % marsh accretion , keep adding to previous amount of accretion  
+   % then convert the rate of m/s to actual accretion m/s*dt in s to get m 
+   slope_in_meter=marsh_vert_rate_insec(iic)*dt ; % each time step adds to accretion  
+   marsh_grow(iic)=marsh_grow_old+slope_in_meter  ;
+   
+   marsh_grow_old=marsh_grow(iic);
+
+  % convert slope in meter to kg/sq.m
+   mo(iic)=slope_in_meter*po ; % added organic mass per time step  ; 
+   bedmass(iic)=bedmass(iic)+mo(iic) ;
+   
+ end  
+% %
+
+figure(1)
+subplot(4,1,1)
+plot(dt_indays, squeeze(dmin2),'r')
+hold on 
+plot(dt_indays, squeeze(dmax2), 'k')
+hold on 
+plot(dt_indays, squeeze(Depth).*(dt_indays./dt_indays), 'g')
+legend('dmin','dmax','zeta')
+
+subplot(4,1,2)
+plot(dt_indays, squeeze(mtr2))
+xlabel('in days')
+ylabel('tidal range')
+
+%figure(2)
+subplot(4,1,3)
+plot(dt_indays, squeeze(marsh_rate_vert)*1000)
+xlabel('in days')
+ylabel('Vertical growth rate in mm/year')
+
+subplot(4,1,4)
+plot(dt_indays,marsh_grow(1:721)*1000) 
+xlabel('in days')
+ylabel('vertical growth in mm')
---
+++ draw_growth_parabola.m
@@ -0,0 +1,96 @@
+clear all ; close all ; clc; 
+% Code to find the parabolic variation of organic biomass with changing
+% changing marsh depth similar to figure 2 of morris et al. 
+%RESPONSES OF COASTAL WETLANDS TO RISING SEA LEVEL
+% Morris, J. T., P. V. Sundareshwar, C. T. Nietch, B. Kjerfve, and    !
+%!  D. R. Cahoon.: Responses of coastal wetlands to rising sea level,   !
+%!  Ecology, 83(10), 2869–2877, 2002. 
+clear all ; close all ; clc ; 
+% iterate through time
+
+% TSK Added
+%Depth=0.5 ; 
+MHW=0.75; 
+mtr=0.5; 
+
+%2*MHW=MTR rougly
+Dmax=-0.737*2*MHW+0.092+MHW; 
+
+Dmin=MHW; 
+Depth=(Dmax+Dmin)*0.5; 
+Dm=linspace(Dmax,Dmin,30); 
+
+%Dmin=MHW ;    % #NOTE THIS IS MHW
+
+%AA=.25*(-Dmin-Dmax)*(Dmax-3*Dmin)  % #scales parabola from 0 to 1 in next line
+
+AA=0.25*(Dmin-Dmax)*(Dmax-Dmin); 
+
+sec2day=1.0/86400; 
+dt=2080; 
+por=.3; % #rough estimate of 1-porosity 
+chiref=0.158; %# recalcitrant carbon
+po=300; % *(1-por)*density ;% 
+
+for iic=1:length(Dm)
+  %disp('dt in days')
+ % dt_indays(iic)=iic*dt*sec2day ;
+  %Depth=0.5; % Dm(d)
+  Depth=Dm(iic); 
+  BMax=2.500; %# kg/m2
+  Bpeak=BMax*(Depth-Dmax)*(Depth-Dmin)/AA  ;% #peak biomass as a function of depth
+  
+  Bpeak(Bpeak<=1e-9)=0  ;
+  Bfrac=(Bpeak/BMax);  % #quick fraction for plotting
+  
+  nuGp=.0138 ; % #below ground  fraction
+% % 
+  %if(dt_indays(iic)<180)
+%   AMC=Bpeak*(nuGp)/(365*24*3600);  %180 growing days
+   AMC=Bpeak*nuGp ;
+   % AMC is units of kg/(sq.m-s)
+  %else
+  % AMC=0.0; 
+  %end 
+  
+  Rref(iic)=AMC*chiref  ;%#what material remains 
+  
+  % Rref is units of kg/(sq.m-year) po is units of kg/m^3 
+  marsh_vert_rate(iic)=(Rref(iic)/po);%  #vertical accretion rate in m/year
+  
+ % O(iic)= O(iic)*dt*1000*365*86400; % accretion every time step in mm . 
+ 
+  Bpeak_save(iic)=Bpeak; 
+  marsh_vert_rate_insec(iic)=marsh_vert_rate(iic)/(365*86400) ; % vertical accretion in mm.s
+ %plot(Depth,Bpeak,'r*')
+%hold on
+end 
+
+figure(1)
+ plot( marsh_vert_rate*1000,Dm*100)
+ xlabel('in mm/year')
+ ylabel('depth below MHW in cm')
+ 
+ 
+ 
+ figure(2)
+ plot(Dm*100, Bpeak_save*1000)
+ ylabel('g/(m^2*year')
+ xlabel('depth ')
+ title('figure 2 / morris et al.')
+ 
+% %
+% figure(1)
+% plot(dt_indays, O) 
+% xlabel('in days')
+% ylabel('in mm') 
+
+% 
+% figure(2)
+% plot(
+% figure(2)
+% plot(O*30*10^6,Dm)
+% 
+% ylabel('number of days')
+% xlabel('vertical growth rate (m/s)')
+% 
